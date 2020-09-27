dcf9663a0cc4ba8bcf4d03dcba6ca019ecac704e
angache@gmail.com <angache@gmail.com>
2020-09-28 01:08:53 +0300

    port değiştirildi

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const bodyParser = require('body-parser');
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+const port = app.listen(process.env.PORT || 3000);
 app.use(bodyParser.json());
 
 app.use('/feed', feedRoutes);
-app.listen(8080);
+app.listen(port, function () {
+  console.log('server started');
+});
